6591593f937b4cef0c97f4c9b5f685862f977d18
Porya Zarei <pzeinstein@gmail.com>
2022-08-17 16:08:05 +0430

    update

--- constants/atlas-config.ts
+++ constants/atlas-config.ts
@@ -1,5 +1,5 @@
-const ATLAS_USERNAME = "spaceomid";
-const ATLAS_PASSWORD = "aaa09157433090@";
+const ATLAS_USERNAME = "spaceomid-2";
+const ATLAS_PASSWORD = "x3ocdTcDKaHgyI5a";
 const ATLAS_DB_NAME = "financial";
 
 // const ATLAS_USERNAME = "space-omid-x";
@@ -10,5 +10,5 @@ export const ATLAS_CONFIG = {
     username: ATLAS_USERNAME,
     password: ATLAS_PASSWORD,
     database: ATLAS_DB_NAME,
-    uri: `mongodb+srv://${ATLAS_USERNAME}:${ATLAS_PASSWORD}@cluster0.4kygakt.mongodb.net/${ATLAS_DB_NAME}?retryWrites=true&w=majority`,
+    uri: `mongodb+srv://${ATLAS_USERNAME}:${ATLAS_PASSWORD}@financial.e71nbei.mongodb.net/${ATLAS_DB_NAME}?retryWrites=true&w=majority`,
 };
