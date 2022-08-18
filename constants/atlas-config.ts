9dca4e0c8247d03bced228247d06befbfe6bd8d8
Porya Zarei <pzeinstein@gmail.com>
2022-08-18 15:47:47 +0430

    improvement

--- constants/atlas-config.ts
+++ constants/atlas-config.ts
@@ -1,12 +1,12 @@
-// const ATLAS_USERNAME = "spaceomid-2";
-// const ATLAS_PASSWORD = "x3ocdTcDKaHgyI5a";
-// const ATLAS_DB_NAME = "financial";
-// const ATLAS_CLUSTER_NAME = "financial.e71nbei";
+const ATLAS_USERNAME = "spaceomid-2";
+const ATLAS_PASSWORD = "x3ocdTcDKaHgyI5a";
+const ATLAS_DB_NAME = "financial";
+const ATLAS_CLUSTER_NAME = "financial.e71nbei";
 
-const ATLAS_USERNAME = "space-omid-x";
-const ATLAS_PASSWORD = "AfSfwX7kilQYFgmU";
-const ATLAS_DB_NAME = "spaceomid";
-const ATLAS_CLUSTER_NAME = "cluster0.4kygakt";
+// const ATLAS_USERNAME = "space-omid-x";
+// const ATLAS_PASSWORD = "AfSfwX7kilQYFgmU";
+// const ATLAS_DB_NAME = "spaceomid";
+// const ATLAS_CLUSTER_NAME = "cluster0.4kygakt";
 
 export const ATLAS_CONFIG = {
     username: ATLAS_USERNAME,
