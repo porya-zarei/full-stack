16d75c80af96cb57b384558dadbe250b0d9a498a
Porya Zarei <pzeinstein@gmail.com>
2022-09-03 01:05:16 +0430

    update urls

--- constants/api.ts
+++ constants/api.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000/";
+// export const BASE_URL = "http://localhost:3000/";
+export const BASE_URL = "https://full-stack-bay.vercel.app/";
 export const API_BASE_URL = BASE_URL + "api";
-// export const API_BASE_URL = "https://full-stack-bay.vercel.app/api";
