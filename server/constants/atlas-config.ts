4b285447d80124016b74876d6cba5212fd73f796
Porya Zarei <pzeinstein@gmail.com>
2023-01-11 15:53:58 +0330

    update atlas config

--- server/constants/atlas-config.ts
+++ server/constants/atlas-config.ts
@@ -16,10 +16,22 @@
 //     uri: `mongodb+srv://${ATLAS_USERNAME}:${ATLAS_PASSWORD}@${ATLAS_CLUSTER_NAME}.mongodb.net/${ATLAS_DB_NAME}?retryWrites=true&w=majority`,
 // };
 
+// LOCAL
+// export const ATLAS_CONFIG = {
+//     username: "ATLAS_USERNAME",
+//     password: "ATLAS_PASSWORD",
+//     database: "ATLAS_DB_NAME",
+//     cluster: "ATLAS_CLUSTER_NAME",
+//     uri: `mongodb://localhost:27017/spaceomid`,
+// };
+
+// arvan db
 export const ATLAS_CONFIG = {
-    username: "ATLAS_USERNAME",
-    password: "ATLAS_PASSWORD",
-    database: "ATLAS_DB_NAME",
-    cluster: "ATLAS_CLUSTER_NAME",
-    uri: `mongodb://localhost:27017/spaceomid`,
+    username: "root",
+    password:
+        "Se0%23T3%23K7%23Nab5%40G5%40H4%23Poh7%23Gi8%40Nyt3%23P0%23Pe9%23Z3%40P3%23Cy6%40H5%40Pel5%40B6%40Fyk5%40Sat3%23Zyp8",
+    database: "spaceomid",
+    cluster: "",
+    // uri: `mongodb://94.101.189.96:27017/spaceomid`,
+    uri: "mongodb://root:Se0%23T3%23K7%23Nab5%40G5%40H4%23Poh7%23Gi8%40Nyt3%23P0%23Pe9%23Z3%40P3%23Cy6%40H5%40Pel5%40B6%40Fyk5%40Sat3%23Zyp8@94.101.189.96:27017/?authMechanism=SCRAM-SHA-256&directConnection=true",
 };
